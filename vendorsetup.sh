c134ba8d319548a772d1357ed0f7a4dd11c552fe
PhatWalrus <harshit.sinha2017@gmail.com>
2024-10-17 11:12:11 +0530

    removed unnecessary stuff and qol changes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,24 +1,43 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2024 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+
 # Target Architecture
-export TARGET_ARCH="arm64-v8a"
+export TARGET_ARCH="arm64"
 
 # About Us
-export FOX_VERSION="R11.1_4"
+export FOX_VERSION="R11.1_6"
 export OF_MAINTAINER="@phobosc_io"
 export FOX_BUILD_TYPE="Stable"
 
 # Build Environment Stuff
 export FOX_BUILD_DEVICE="Pong"
 export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 export TARGET_DEVICE_ALT="Pong,pong"
 export FOX_TARGET_DEVICES="Pong,pong"
 export TW_DEFAULT_LANGUAGE="en"
 export LC_ALL="C"
+export OF_OPTIONS_LIST_NUM=8
 
 # Use Magisk Boot for Patching
 export OF_USE_MAGISKBOOT=1
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 
 # We Have A/B Partitions
 export FOX_AB_DEVICE=1
@@ -33,16 +52,14 @@ export OF_ALLOW_DISABLE_NAVBAR=0
 export OF_CLOCK_POS=0
 export OF_SCREEN_H=2400
 export OF_STATUS_H=120
-
+export FOX_USE_SPECIFIC_MAGISK_ZIP=/home/toor/Magisk-v28.0.apk
 # Device Stuff
-export OF_KEEP_FORCED_ENCRYPTION=1
 export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 export OF_FBE_METADATA_MOUNT_IGNORE=1
 export OF_PATCH_AVB20=1
 export FOX_USE_NANO_EDITOR=1
 export FOX_ENABLE_APP_MANAGER=0
 export FOX_DELETE_AROMAFM=1
-export OF_KEEP_DM_VERITY=1
 export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
 export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
 export OF_NO_ADDITIONAL_MIUI_PROPS_CHECK=1
@@ -51,16 +68,11 @@ export OF_USE_LEGACY_BATTERY_SERVICES=1
 export FOX_USE_UPDATED_MAGISKBOOT=1
 export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
 export OF_DISABLE_OTA_MENU=1
-export OF_SUPPORT_ALL_PAYLOAD_OTA_UPDATES=1
 export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 export OF_DYNAMIC_FULL_SIZE=7516192768
 
-# Necessary Workaround
-export FOX_BUGGED_AOSP_ARB_WORKAROUND="1601559499"
-
 # Quick Backup List
 export FOX_R11=1
-export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=0
 export OF_QUICK_BACKUP_LIST="/boot;/dtbo;/data;/system_image;/vendor_image;"
 
 # Add Some Extras
